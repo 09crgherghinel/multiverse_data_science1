ebed99d017d8a96af90663d0030b85e79a567f70
09crgherghinel <102657737+09crgherghinel@users.noreply.github.com>
2022-03-30 12:07:04 +0100

    Day 2 notebook

---
+++ FirstPythonNotebook.py
@@ -0,0 +1,13 @@
+print('Hello World!')
+#This is a cell
+#Assign a variable
+v = 1
+print(v)
+
+#Assign something else
+ds_one= 'DS1 is awesome!'
+print(ds_one)
+
+m = 'm comes before n'
+print(m)
+45 - 19
